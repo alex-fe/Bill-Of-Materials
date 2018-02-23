f94698ca2e648aef4e4ce261fe17439f7a73b6b3
Alex Feldman <alexfeldman93@gmail.com>
2018-02-22 16:02:56 -0800

    Added requirements.txt, tests file, and updated Formatter in Utils

--- utils.py
+++ utils.py
@@ -8,27 +8,50 @@ class Colors:
 
     @classmethod
     def success(cls, message):
-        print '{}{}{}'.format(cls.OKGREEN, message, cls.ENDC)
+        print('{}{}{}'.format(cls.OKGREEN, message, cls.ENDC))
 
     @classmethod
     def warning(cls, message):
-        print '{}{}{}'.format(cls.WARNING, message, cls.ENDC)
+        print('{}{}{}'.format(cls.WARNING, message, cls.ENDC))
 
     @classmethod
     def error(cls, message):
-        print '{}{}{}'.format(cls.FAIL, message, cls.ENDC)
+        print('{}{}{}'.format(cls.FAIL, message, cls.ENDC))
 
-class FormatterError(Error):
+class FormatterError(Exception):
     pass
 
 class Formatter(object):
     """Convert file/file as string to DataFrame"""
 
     columns = ('Location', 'Part', 'Quantity', 'Notes')
-    final_list = pd.DataFrame(columns=columns)
 
-    def __init__(self, file_=None, file_as_string=None):
+    def __call__(self, file_=None, file_as_string=None):
+        """When Formatter is called, convert file/file_as_string to DataFrame.
+        Args:
+            file_(file): BOM as file.
+            file_as_string (string): BOM as string.
+
+        Returns:
+            df (pd.DataFrame)
+        """
         if not (file_ or file_as_string):
             raise FormatterError
         if file_:
             #TODO convert to file_as_string
+            pass
+        df = self.create_frame(file_as_string)
+        return df
+
+    @staticmethod
+    def remove_whitespace(string, join=False):
+        #FIXME: too broad, needs to be more specific
+        """Return file as long string without spaces or new lines."""
+        string = string.split(sep=None)
+        if join:
+            return ' '.join(string)
+        return string
+
+    def create_frame(self, file_as_string):
+        for list_item in self.remove_whitespace(file_as_string):
+            print(list_item)
