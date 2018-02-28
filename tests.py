8e56a98769008bfb0035eb89b138bd91346eb0f9
Alex Feldman <alexfeldman93@gmail.com>
2018-02-28 15:26:34 -0800

    Finished Q tests

--- tests.py
+++ tests.py
@@ -1,5 +1,8 @@
 import unittest
 
+import pandas as pd
+import pandas.util.testing as pdt
+
 from utils import Formatter
 
 # utils
@@ -12,30 +15,38 @@ class TestFormatter(unittest.TestCase):
         R87,R88,R89
         1k
         3
-        R2,R5,R8,R11,R14,R17,R20,R23,R26,R29,R32,R35,R38,R41,R44
+        R2,R5,R8
         1.8k
         24"""
         self.correct = [
             'R80b,R80b', '680', '2',
             'R87,R88,R89', '1k', '3',
-            'R2,R5,R8,R11,R14,R17,R20,R23,R26,R29,R32,R35,R38,R41,R44',
-            '1.8k', '24'
+            'R2,R5,R8', '1.8k', '24'
         ]
+        self.df = pd.DataFrame(
+            data={
+                'Location': ['R80b,R80b', 'R87,R88,R89', 'R2,R5,R8'],
+                'Part': ['680', '1k', '1.8k'],
+                'Quantity': ['2', '3', '24']
+            }
+        )
 
     def test_to_list(self):
         """Test string is properly split into list."""
         result = Formatter.to_list(self.string_as_list)
-        self.assertEquals(result, self.correct)
+        self.assertEqual(result, self.correct)
 
     def test_to_list_as_string(self):
         """Test string is properly split into joined list as type str."""
         correct = ' '.join(self.correct)
         result = Formatter.to_list(self.string_as_list, as_string=True)
-        self.assertEquals(result, correct)
+        self.assertEqual(result, correct)
         self.assertIsInstance(result, str)
 
     def test_create_frame(self):
+        """Test correct DataFrame is produced."""
         result = Formatter.create_frame(self.string_as_list)
+        pdt.assert_frame_equal(result, self.df)
 
 
 if __name__ == '__main__':
